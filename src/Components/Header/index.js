928ee2b8ad85287cbface7c6697583a3e3a7ab19
KRISHNACHAND <keestuchand@gmail.com>
2021-08-14 12:44:03 +0530

    new push

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,61 +1,65 @@
-import React, { Component } from 'react';
-import './index.css';
-import { Row, Col } from 'reactstrap';
+import React, { Component } from "react";
+import "./index.css";
+import { Row, Col } from "reactstrap";
 class Header extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			init: '',
-			content: [ 'Web developer', 'Designer', 'Photo editor', 'Architecture' ]
-		};
-		setTimeout(() => {
-			this.renderContent();
-		}, 1000);
-	}
+    this.state = {
+      init: "",
+      content: ["Web developer", "Designer", "Photo editor", "Architecture"],
+    };
+    setTimeout(() => {
+      this.renderContent();
+    }, 1000);
+  }
 
-	renderContent = () => {
-		this.setState({
-			init: this.state.content[Math.floor(Math.random() * this.state.content.length)]
-		});
-		setTimeout(() => {
-			this.renderContent();
-		}, 500);
-	};
+  renderContent = () => {
+    this.setState({
+      init: this.state.content[
+        Math.floor(Math.random() * this.state.content.length)
+      ],
+    });
+    setTimeout(() => {
+      this.renderContent();
+    }, 500);
+  };
 
-	render() {
-		return (
-			<div className="header">
-				<div className="container pt-5">
-					<Row>
-						<Col xl={12}>
-							<h3 className="display-4 text-light">ambience</h3>
-							<h2 className="display-3 text-light">Krishnachand Kannan</h2>
-							<p className="ui-para text-light">Beginner as a {this.state.init}</p>
-							<button
-								className="btn-secondry"
-								onClick={() => {
-									window.scroll({
-										behavior: 'smooth',
-										top: 660
-									});
-								}}
-							>
-								Read me
-							</button>
-							<a
-								href="https://drive.google.com/file/d/1TNOdjZkY2CpNWZP3i_uH87WVqrwAZzEA/view?usp=sharing"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<button className="btn-secondry">See Resume</button>
-							</a>
-						</Col>
-					</Row>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="header">
+        <div className="container pt-5">
+          <Row>
+            <Col xl={12}>
+              <h3 className="display-4 text-light">ambience</h3>
+              <h2 className="display-3 text-light">Krishnachand Kannan</h2>
+              <p className="ui-para text-light">
+                Beginner as a {this.state.init}
+              </p>
+              <button
+                className="btn-secondry"
+                onClick={() => {
+                  window.scroll({
+                    behavior: "smooth",
+                    top: 660,
+                  });
+                }}
+              >
+                Read me
+              </button>
+              <a
+                href="https://drive.google.com/file/d/1TNOdjZkY2CpNWZP3i_uH87WVqrwAZzEA/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn-secondry">See Resume</button>
+              </a>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/Components/Work/index.js
+++ src/Components/Work/index.js
@@ -1,38 +1,40 @@
-import React from 'react';
-import './index.css';
-import { Row, Col } from 'reactstrap';
-import VGT from '../../img/vgt.PNG';
-import cloudix from '../../img/cloudix.png';
-import hr from '../../img/HRblusky.png';
-import RoaDo from '../../img/RoaDo.png'
+import React from "react";
+import "./index.css";
+import { Row, Col } from "reactstrap";
+import VGT from "../../img/vgt.PNG";
+import cloudix from "../../img/cloudix.png";
+import hr from "../../img/HRblusky.png";
+import RoaDo from "../../img/RoaDo.png";
 const Work = () => {
-	return (
-		<div className="work">
-			<div className="text-uppercase position-sticky">
-				<h3 className="text-light text-center display-4">Intern & Work</h3>
-			</div>
-			<div className="container">
-				<Row className="img-block">
-					<Col md={6} lg={3} className="text-center">
-						<img src={VGT} loading="eager" alt="VGT" />
-						<p className="text-muted">Virtualgodown Technologies. </p>
-					</Col>
-					<Col md={6} lg={3} className="text-center">
-						<img src={cloudix} loading="eager" alt="cloudix" />
-						<p className="text-muted mt-4">Jaishu Consulting Pvt Ltd, (now Cloudix at Chennai).</p>
-					</Col>
-					<Col md={6} lg={3} className="text-center">
-						<img src={hr} loading="eager" alt="HR" />
-						<p className="text-muted">HRBluSky</p>
-					</Col>
-					<Col md={6} lg={3} className="text-center">
-						<img src={RoaDo} loading="eager" alt="HR" />
-						<p className="text-muted">RoaDo, Bangalore</p>
-					</Col>
-				</Row>
-			</div>
-		</div>
-	);
+  return (
+    <div className="work">
+      <div className="text-uppercase position-sticky">
+        <h3 className="text-light text-center display-4">Intern & Work</h3>
+      </div>
+      <div className="container">
+        <Row className="img-block">
+          <Col md={6} lg={3} className="text-center">
+            <img src={VGT} loading="eager" alt="VGT" />
+            <p className="text-muted">Virtualgodown Technologies. </p>
+          </Col>
+          <Col md={6} lg={3} className="text-center">
+            <img src={cloudix} loading="eager" alt="cloudix" />
+            <p className="text-muted mt-4">
+              Jaishu Consulting Pvt Ltd, (now Cloudix at Chennai).
+            </p>
+          </Col>
+          <Col md={6} lg={3} className="text-center">
+            <img src={hr} loading="eager" alt="HR" />
+            <p className="text-muted">HRBluSky</p>
+          </Col>
+          <Col md={6} lg={3} className="text-center">
+            <img src={RoaDo} loading="eager" alt="HR" />
+            <p className="text-muted">RoaDo, Bangalore</p>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
